5cfb73e6a6e64a198536abd740e1b058c4d7298c
deepedes <deep.manit@gmail.com>
2020-06-20 20:03:25 +0530

    reader write lock

---
+++ src/Threading/ReadWriteLock.java
@@ -0,0 +1,31 @@
+package Threading;
+
+public class ReadWriteLock {
+    boolean isWriteLocked = false;
+    int readers = 0;
+    public synchronized void acquireReadLock() throws InterruptedException {
+        while (isWriteLocked == false)
+            wait();
+
+        ++readers;
+    }
+
+    public synchronized void releaseReadLock() {
+        readers--;
+        notify();
+    }
+
+    public synchronized void acquireWriteLock() throws InterruptedException {
+        while (isWriteLocked || readers != 0) {
+            wait();
+        }
+
+        isWriteLocked = true;
+
+    }
+
+    public synchronized void releaseWriteLock() {
+        isWriteLocked = false;
+        notify();
+    }
+}
