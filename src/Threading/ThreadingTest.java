4e4f813b5cf71b991ca1503fceb898e20f019c92
deepedes <deep.manit@gmail.com>
2020-06-21 05:39:25 +0530

    barberhaircut and DinningPhilospher

--- src/Threading/ThreadingTest.java
+++ src/Threading/ThreadingTest.java
@@ -2,6 +2,7 @@ package Threading;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.BrokenBarrierException;
 
 public class ThreadingTest {
     public static void test() throws InterruptedException {
@@ -13,7 +14,62 @@ public class ThreadingTest {
    //     runTestUnisexBathroom();
      //   testSequence();
        // testFooBar();
-        testZeroOnTwo();
+       // testZeroOnTwo();
+        runTestUber();
+    }
+
+    private static void runTestUber() throws InterruptedException {
+
+
+        final UberSeatingProblem uberSeatingProblem = new UberSeatingProblem();
+        Set<Thread> allThreads = new HashSet<Thread>();
+
+        for (int i = 0; i < 10; i++) {
+
+            Thread thread = new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        uberSeatingProblem.seatDemocrat();
+                    } catch (InterruptedException ie) {
+                        System.out.println("We have a problem");
+
+                    } catch (BrokenBarrierException bbe) {
+                        System.out.println("We have a problem");
+                    }
+
+                }
+            });
+            thread.setName("Democrat_" + (i + 1));
+            allThreads.add(thread);
+
+            Thread.sleep(50);
+        }
+
+        for (int i = 0; i < 14; i++) {
+            Thread thread = new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        uberSeatingProblem.seatRepublican();
+                    } catch (InterruptedException ie) {
+                        System.out.println("We have a problem");
+
+                    } catch (BrokenBarrierException bbe) {
+                        System.out.println("We have a problem");
+                    }
+                }
+            });
+            thread.setName("Republican_" + (i + 1));
+            allThreads.add(thread);
+            Thread.sleep(20);
+        }
+
+        for (Thread t : allThreads) {
+            t.start();
+        }
+
+        for (Thread t : allThreads) {
+            t.join();
+        }
     }
 private static void testZeroOnTwo() throws InterruptedException
     {
